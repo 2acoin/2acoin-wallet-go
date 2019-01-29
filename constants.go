7cb129fa7f2066386e22bbafdef57c41d675cb21
2ACoin Miner <miner@2acoin.org>
2019-01-29 12:56:32 -0600

    Build v1.0.5 for 2ACoin v1.1.5

--- constants.go
+++ constants.go
@@ -7,7 +7,7 @@
 package main
 
 const (
-	versionNest                 = "1.0.4"
+	versionNest                 = "1.0.5"
 	urlCryptoComparexCoin       = "https://api.coingecko.com/api/v3/simple/price?ids=2acoin&vs_currencies=USD"
 	logFileFilename             = "2ACoin-WalletGo.log"
 	urlBlockExplorer            = "https://explorer.2acoin.org/"
